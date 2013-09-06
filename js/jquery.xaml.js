d0d8adf87f2ddab9483e49ae27551377afb52e6f
KimSungWook <kimsungwook@witstudio.net>
2013-09-06 14:26:30 +0900

    stackpanel vertical-alignment 완료.

--- js/jquery.xaml.js
+++ js/jquery.xaml.js
@@ -7,31 +7,31 @@
 */
 
 function resolveTag(srcTagName) {
-        switch(srcTagName.toLowerCase()) {
-                case 'stackpanel':
-                        replaceTag(srcTagName, 'ul').addClass(srcTagName).replaceCustomAttr().childrenWrap('li').replaceChildrenCustomAttr();
-                        //return replaceTag(srcTagName, 'ul').addClass(srcTagName).replaceCustomAttr().restoreAttr().childrenWrap('li');
-        }
+	switch(srcTagName.toLowerCase()) {
+		case 'stackpanel':
+			replaceTag(srcTagName, 'ul').addClass(srcTagName).replaceCustomAttr().childrenWrap('li').replaceChildrenCustomAttr();
+			//return replaceTag(srcTagName, 'ul').addClass(srcTagName).replaceCustomAttr().restoreAttr().childrenWrap('li');
+	}
 }
 
 jQuery.fn.replaceChildrenCustomAttr = function () {
-        $(this).children().each(function () {
-                $(this).replaceChildrenCustomAttr();
-        });
-        $(this).each(function() {
-                $(this).replaceCustomAttr();
-        });
+	$(this).children().each(function () {
+			$(this).replaceChildrenCustomAttr();
+	});
+	$(this).each(function() {
+			$(this).replaceCustomAttr();
+	});
 }
 
 
 function replaceTag(srcTagName, destTagName) {
-        var newTag = null;
-        $(srcTagName).replaceWith(function(){
-                newTag = $("<"+destTagName+" />", {html: $(this).html(), data : $(this).data()});
-                newTag.attr($(this).allAttr());
-                return newTag;
-        });
-        return newTag;
+	var newTag = null;
+	$(srcTagName).replaceWith(function(){
+		newTag = $("<"+destTagName+" />", {html: $(this).html(), data : $(this).data()});
+		newTag.attr($(this).allAttr());
+		return newTag;
+	});
+	return newTag;
 }
 
 /*
@@ -42,42 +42,48 @@ jQuery.fn.restoreAttr = function() {
 */
 
 jQuery.fn.allAttr = function() {
-        var arr = new Object();
-        for(var i = 0; i <  this[0].attributes.length; i++) {
-                // skip custom attributes
-                //if(this[0].attributes[i].nodeName.indexOf('data-') == 0) { continue; }
-                arr[this[0].attributes[i].nodeName] = this[0].attributes[i].nodeValue;
-        }
-        return arr;
+	var arr = new Object();
+	for(var i = 0; i <  this[0].attributes.length; i++) {
+		// skip custom attributes
+		//if(this[0].attributes[i].nodeName.indexOf('data-') == 0) { continue; }
+		arr[this[0].attributes[i].nodeName] = this[0].attributes[i].nodeValue;
+	}
+	return arr;
 };
 
 
 jQuery.fn.replaceCustomAttr = function () {
-        for(var key in $(this).data()) {
-                switch(key.toLowerCase()) {
-                        case 'background':
-                                $(this).css("background-color", $(this).data()[key]);
-                                break;
-                        case 'width':
-                                $(this).css("width", $(this).data()[key]);
-                                break;
-                        case 'height':
-                                $(this).css("height", $(this).data()[key]);
-                                break;
-                        case 'horizontalalignment':
-                                $(this).parent().css("text-align", $(this).data()[key].toLowerCase());
-                                break;
-                        default:
-                                $(this).addClass(key + "-" + $(this).data()[key]);
-                }
-                $(this).removeAttr('data-' + key);
-        }
-        return this;
+	for(var key in $(this).data()) {
+		switch(key.toLowerCase()) {
+			case 'background':
+				$(this).css("background-color", $(this).data()[key]);
+				break;
+			case 'width':
+				$(this).css("width", $(this).data()[key]);
+				break;
+			case 'height':
+				$(this).css("height", $(this).data()[key]);
+				break;
+			case 'horizontalalignment':
+				$(this).parent().css("text-align", $(this).data()[key].toLowerCase());
+				break;
+			case 'verticalalignment':
+				var value = $(this).data()[key].toLowerCase();
+				value = (value == 'center' ? "middle" : value);
+				$(this).css("vertical-align", value);
+				break;
+			default:
+				$(this).addClass(key + "-" + $(this).data()[key]);
+		}
+		$(this).removeAttr('data-' + key);
+	}
+	return this;
 };
 
+
 jQuery.fn.childrenWrap = function (wrapTagName) {
-        return $(this).children().each( function () {
-                return $(this).wrap("<"+wrapTagName+" />");
-        });
-        return newTag;
+	return $(this).children().each( function () {
+		return $(this).wrap("<"+wrapTagName+" />");
+	});
+	return newTag;
 };
